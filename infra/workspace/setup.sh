3f64e22eae1eb0d7cffaaea8d17fc07f5468a684
Phil Ruff <phil.ruff@gmail.com>
2019-03-28 16:35:50 -0400

    initial commit

---
+++ infra/workspace/setup.sh
@@ -0,0 +1,10 @@
+# setting up the env to 
+brew install chefdk
+brew install docker
+brew install docker-machine
+brew install docker-compose
+brew install virtualbox
+brew install vagrant
+choco install chefdk
+
+vagrant plugin install vagrant-berkshelf
